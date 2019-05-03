8347f489aa81daddc9cb8347177575b75065f9b8
pangpada <38246310+pangpada@users.noreply.github.com>
2019-05-03 19:11:20 +0700

    update nav-logout

--- update-employee.php
+++ update-employee.php
@@ -13,6 +13,14 @@
     <title>Update Employee</title>
 </head>
 <body>
+
+    <nav class="navbar navbar-light bg-light justify-content-between">
+                <a class="navbar-brand">CLOTHES SHOP</a>
+                <form class="form-inline">
+                  <button class="btn btn-outline-danger" type="submit">logout</button>
+                </form>
+    </nav>
+
     <div class="container">
 
         <div class="container-form">
