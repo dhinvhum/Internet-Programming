d28af170eadd61fb1baec15e102276d1c07e9070
dhinvhum <dhinbhum@gmail.com>
2019-05-04 20:59:09 +0700

    update set order

--- order-product-set.php
+++ order-product-set.php
@@ -2,39 +2,69 @@
 <?php
 
     //SET VARIABLE
-    $type = $_POST['shirt'];
-    $color = $_POST['color'];
-    $size = $_POST['size'];
-    $quan = $_POST['quan'];
+    $I1 = $_POST['ID1'];
+    $I2 = $_POST['ID2'];
+    $I3 = $_POST['ID3'];
+    $I4 = $_POST['ID4'];
+    $I5 = $_POST['ID5'];
+    $I6 = $_POST['ID6'];
+    $I7 = $_POST['ID7'];
+    $I8 = $_POST['ID8'];
+    $I9 = $_POST['ID9'];
+    $I10 = $_POST['ID10'];
 
-    echo $type,$color,$size,$quan;
-
-    $connect = mysqli_connect("localhost", "root", "", "store");
-    $sqlck = "SELECT Quantity FROM product WHERE Product='$type' AND Color='$color' AND Size='$size'";
-    $resultck = mysqli_query($connect, $sqlck);
-    $row = mysqli_fetch_assoc($resultck);
-
-    echo $row['Quantity'];
-
-    if ($quan>$row['Quantity']) {
-        echo 'Can not order';
-    } else {
-
-        $sql = "UPDATE product SET Quantity=Quantity-'$quan' WHERE Product='$type' AND Color='$color' AND Size='$size' ";
+    echo $I1;
+        $connect = mysqli_connect("localhost", "root", "", "store");
+        $sql = "SELECT * FROM product";
         $result = mysqli_query($connect, $sql);
 
-        if(!$result) {
-            echo mysqli_error().'<br>';
-            die('Can not access database!');
-        } else {
-            header("location:order-product.php");
+        while($row = mysqli_fetch_assoc($result)) {
+            if($row['ID']=='1') {
+                $sql1 = 'UPDATE product SET Quantity=Quantity-"'.$I1.'" WHERE ID = "'.$row['ID'].'" ';
+                $result1 = mysqli_query($connect, $sql1);
+            }
+            if($row['ID']=='2') {
+                $sql1 = 'UPDATE product SET Quantity=Quantity-"'.$I2.'" WHERE ID = "'.$row['ID'].'" ';
+                $result1 = mysqli_query($connect, $sql1);
+            }
+            if($row['ID']=='3') {
+                $sql1 = 'UPDATE product SET Quantity=Quantity-"'.$I3.'" WHERE ID = "'.$row['ID'].'" ';
+                $result1 = mysqli_query($connect, $sql1);
+            }
+            if($row['ID']=='4') {
+                $sql1 = 'UPDATE product SET Quantity=Quantity-"'.$I4.'" WHERE ID = "'.$row['ID'].'" ';
+                $result1 = mysqli_query($connect, $sql1);
+            }
+            if($row['ID']=='5') {
+                $sql1 = 'UPDATE product SET Quantity=Quantity-"'.$I5.'" WHERE ID = "'.$row['ID'].'" ';
+                $result1 = mysqli_query($connect, $sql1);
+            }
+            if($row['ID']=='6') {
+                $sql1 = 'UPDATE product SET Quantity=Quantity-"'.$I6.'" WHERE ID = "'.$row['ID'].'" ';
+                $result1 = mysqli_query($connect, $sql1);
+            }
+            if($row['ID']=='7') {
+                $sql1 = 'UPDATE product SET Quantity=Quantity-"'.$I7.'" WHERE ID = "'.$row['ID'].'" ';
+                $result1 = mysqli_query($connect, $sql1);
+            }
+            if($row['ID']=='8') {
+                $sql1 = 'UPDATE product SET Quantity=Quantity-"'.$I8.'" WHERE ID = "'.$row['ID'].'" ';
+                $result1 = mysqli_query($connect, $sql1);
+            }
+            if($row['ID']=='9') {
+                $sql1 = 'UPDATE product SET Quantity=Quantity-"'.$I9.'" WHERE ID = "'.$row['ID'].'" ';
+                $result1 = mysqli_query($connect, $sql1);
+            }
+            if($row['ID']=='10') {
+                $sql1 = 'UPDATE product SET Quantity=Quantity-"'.$I10.'" WHERE ID = "'.$row['ID'].'" ';
+                $result1 = mysqli_query($connect, $sql1);
+            }
+            
         }
 
-        
-
-    }
+            header("location:order-product.php"); 
+    
+    mysqli_close($connect);
 
     
-
-    mysqli_close($connect);
 ?>
